cbb87daebc45c8250aff291ab4199cb9182a2a9f
mario73323 <56419854+mario73323@users.noreply.github.com>
2019-11-05 22:11:42 -0500

    cola

---
+++ PilaCola/PilaCola/ClasesCola/Cola.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PilaCola.ClasesPila;
+
+namespace PilaCola.ClasesCola
+{
+    public class Cola
+    {
+        private NodoCola inicio = new NodoCola();
+        private NodoCola ultimo = new NodoCola();
+        public Cola()
+        {
+            inicio = null;
+            ultimo = null;
+        }
+        public void Insertarcola()
+        {
+            NodoCola ingresa = new NodoCola();
+            Console.WriteLine("Ingrese el dato para la cola");
+            ingresa.Datocola = IngresarDatocola();
+            if (VaciaCola())
+            {
+                ingresa.Siguientecola = inicio;
+                inicio = ingresa;
+                ultimo = ingresa;
+            }
+            else
+            {
+                ingresa.Siguientecola = null;
+                ultimo.Siguientecola = ingresa;
+                ultimo = ingresa;
+            }
+            Console.WriteLine("Dato Ingresado");
+
+        }
+        public void Mostrarcola()
+        {
+            NodoCola temp = new NodoCola();
+            temp = inicio;
+            if (!VaciaCola())
+            {
+                while (temp != null)
+                {
+                    Console.WriteLine("  " + temp.Datocola);
+                    temp = temp.Siguientecola;
+                }
+            }
+            else
+                Console.WriteLine("La cola esta Vacia");
+        }
+        public void Eliminarcola()
+        {
+            if (!VaciaCola())
+            {
+                Console.WriteLine("Se elimino el numero " + inicio.Datocola);
+                inicio = inicio.Siguientecola;
+            }
+            else
+                Console.WriteLine("La cola esta vacia");
+        }
+        public void Vaciarcola()
+        {
+            while (!VaciaCola())
+            {
+                Eliminarcola();
+            }
+        }
+        public Boolean VaciaCola()
+        {
+            if (inicio == null)
+                return true;
+            else
+                return false;
+        }
+        protected int IngresarDatocola()
+        {
+            string num = Console.ReadLine();
+            bool error = int.TryParse(num, out int numero);
+            if (!error)
+            {
+                Console.WriteLine("Solo se aceptan numeros enteros");
+                numero = IngresarDatocola();
+            }
+            return numero;
+        }
+    }
+}
--- PilaCola/PilaCola/Program.cs
+++ PilaCola/PilaCola/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PilaCola.ClasesPila;
-//using PilaCola.ClasesCola;
+using PilaCola.ClasesCola;
 
 namespace PilaCola
 {
@@ -12,44 +12,104 @@ namespace PilaCola
     {
         static void Main(string[] args)
         {
-            //Pila
-            
-            Pila pila = new Pila();
-            bool ciclo = false;
-            while (ciclo == false)
+            bool ciclo = false,opcion=false;
+            while (opcion == false)
             {
-                Console.WriteLine("Escoja una accion para la Pila \n1.-Ingresar Dato \n2.-Eliminar Dato " +
-                "\n3.-Vaciar Pila \n4.-Imprimir Pila \n5.-Salir");
-                string op = Console.ReadLine();
-                switch (op)
+                Console.WriteLine("Escoja un tipo de estructura \n1.-Pila \n2.-Cola \n3.-Salir");
+                string estructura = Console.ReadLine();
+                switch (estructura)
                 {
                     case "1":
-                        pila.Insertar();
-                        Console.WriteLine("Presione un tecla para continuar");
-                        Console.ReadKey();
+                        Pila pila = new Pila();
+                        ciclo = false;
+                        while (ciclo == false)
+                        {
+                            Console.WriteLine("Escoja una accion para la Pila \n1.-Ingresar Dato \n2.-Eliminar Dato " +
+                            "\n3.-Vaciar Pila \n4.-Imprimir Pila \n5.-Salir");
+                            string op = Console.ReadLine();
+                            switch (op)
+                            {
+                                case "1":
+                                    pila.Insertar();
+                                    Console.WriteLine("Presione un tecla para continuar");
+                                    Console.ReadKey();
+                                    break;
+                                case "2":
+                                    pila.Eliminar();
+                                    Console.WriteLine("Presione un tecla para continuar");
+                                    Console.ReadKey();
+                                    break;
+                                case "3":
+                                    pila.Vaciar();
+                                    Console.WriteLine("Presione un tecla para continuar");
+                                    Console.ReadKey();
+                                    break;
+                                case "4":
+                                    pila.Mostrar();
+                                    Console.WriteLine("Presione un tecla para continuar");
+                                    Console.ReadKey();
+                                    break;
+                                case "5":
+                                    ciclo = true;
+                                    break;
+                                default:
+                                    Console.WriteLine("No se Escogio ninguna opcion, escoja una opcion valida");
+                                    Console.WriteLine("Presione un tecla para continuar");
+                                    break;
+                            }
+                            Console.Clear();
+                        }
                         break;
                     case "2":
-                        pila.Eliminar();
-                        Console.WriteLine("Presione un tecla para continuar");
-                        Console.ReadKey();
+                        //cola
+                        Cola cola = new Cola();
+                        ciclo = false;
+                        while (ciclo == false)
+                        {
+                            Console.WriteLine("Escoja una accion para la cola \n1.-Ingresar Dato \n2.-Eliminar Dato " +
+                            "\n3.-Vaciar cola \n4.-Imprimir cola \n5.-Salir");
+                            string op = Console.ReadLine();
+                            switch (op)
+                            {
+                                case "1":
+                                    cola.Insertarcola();
+                                    Console.WriteLine("Presione un tecla para continuar");
+                                    Console.ReadKey();
+                                    break;
+                                case "2":
+                                    cola.Eliminarcola();
+                                    Console.WriteLine("Presione un tecla para continuar");
+                                    Console.ReadKey();
+                                    break;
+                                case "3":
+                                    cola.Vaciarcola();
+                                    Console.WriteLine("Presione un tecla para continuar");
+                                    Console.ReadKey();
+                                    break;
+                                case "4":
+                                    cola.Mostrarcola();
+                                    Console.WriteLine("Presione un tecla para continuar");
+                                    Console.ReadKey();
+                                    break;
+                                case "5":
+                                    ciclo = true;
+                                    break;
+                                default:
+                                    Console.WriteLine("No se Escogio ninguna opcion, escoja una opcion valida");
+                                    Console.WriteLine("Presione un tecla para continuar");
+                                    break;
+                            }
+                            Console.Clear();
+                        }
                         break;
                     case "3":
-                        pila.Vaciar();
-                        Console.WriteLine("Presione un tecla para continuar");
-                        Console.ReadKey();
-                        break;
-                    case "4":
-                        pila.Mostrar();
-                        Console.WriteLine("Presione un tecla para continuar");
-                        Console.ReadKey();
-                        break;
-                    case "5":
-                        ciclo = true;
+                        opcion = true;
                         break;
                     default:
                         Console.WriteLine("No se Escogio ninguna opcion, escoja una opcion valida");
                         Console.WriteLine("Presione un tecla para continuar");
                         break;
+
                 }
                 Console.Clear();
             }
@@ -57,3 +117,4 @@ namespace PilaCola
         }
     }
 }
+
